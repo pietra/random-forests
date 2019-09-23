9335c682f8d01e7127c254da74de4fe200cdc0c9
Pietra Thamy Sofia Da Fonseca Freitas <pietrasofia@hotmail.com>
2019-09-23 18:11:30 -0300

    Move classes Node e Leaf.

--- decision_tree.py
+++ decision_tree.py
@@ -1,49 +1,8 @@
 from dataset_reader import read_dataset
 from collections import Counter
 import random
-from random import random as random_digit
-from dataclasses import dataclass
 
-
-@dataclass
-class Leaf:
-    which_class: str
-    value: str
-
-    def __init__(self, which_class=None, value=None):
-        self.which_class = which_class
-        self.value = value
-
-    def print_leaf(self, level=0):
-        hifen = ""
-        for i in range(level):
-            hifen = hifen + "-----"
-        print(hifen, "which_class: ", self.which_class, ", value: ", self.value)
-
-
-@dataclass
-class Node:
-    attribute: str
-    children: list
-
-    def __init__(self, value=None, attribute=None):
-        self.attribute = attribute
-        self.children = []
-
-    def add_child(self, child):
-        self.children.append(child)
-
-    def print_node(self, level=0):
-        hifen = ""
-        for i in range(level):
-            hifen = hifen + "-----"
-        print(hifen, "attribute: ", self.attribute)
-        print(hifen, "children: ")
-        for child in self.children:
-            if(isinstance(child, Node)):
-                print(child.print_node(level+1))
-            else:
-                print(child.print_leaf(level+1))
+from tree import Leaf, Node
 
 
 def main():
