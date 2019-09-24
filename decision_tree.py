1dab4a88d63e044237e6ee45e3e40c2586398512
Pietra Thamy Sofia Da Fonseca Freitas <pietrasofia@hotmail.com>
2019-09-24 09:39:48 -0300

    Altera biblioteca de árvore utilizada.

--- decision_tree.py
+++ decision_tree.py
@@ -2,26 +2,27 @@ from dataset_reader import read_dataset
 from collections import Counter
 import random
 
-from tree import Leaf, Node
+from anytree import Node, RenderTree
 
 
 def main():
     dataset, attributes = read_dataset("dataset_joga_tenis.csv")
     attributes.remove('class')
-    tree = decision_tree(dataset, attributes)
-    tree.print_node()
+    tree = Node("Decision Tree")
+    final_decision_tree = decision_tree(dataset, attributes, tree)
+    print_tree(final_decision_tree)
 
 
-def decision_tree(dataset, attributes, value=None):
+def decision_tree(dataset, attributes, father=None):
     # Only one class for that dataset
     some_class = only_one_class(dataset)
     if some_class:
-        return Leaf(which_class=some_class, value=value)
+        Node(some_class, parent=father)
 
     # No more attributes
     elif not attributes:
         classes = dataset["class"].values.tolist()
-        return Leaf(which_class=most_frequent(classes), value=value)
+        Node(most_frequent(classes), parent=father)
 
     else:
         # Choose a attribute
@@ -31,19 +32,20 @@ def decision_tree(dataset, attributes, value=None):
         values_of_that_attribute = remove_repeated_values_of_list(
             values_of_that_attribute)
 
-        new_node = Node(attribute=attribute)
+        # Create attribute node
+        new_node = Node(attribute, parent=father)
 
         # For each value of that attribute
         for value in values_of_that_attribute:
             dataset_filtered_by_value = dataset[dataset[attribute] == value]
+            value_node = Node(value, parent=new_node)
             # If there are no cases for that value
             if dataset_filtered_by_value.empty:
                 classes = dataset["class"].values.tolist()
-                new_node.add_child(
-                    Leaf(which_class=most_frequent(classes), value=value))
+                Node(most_frequent(classes), parent=value_node)
             else:
-                new_node.add_child(decision_tree(
-                    dataset_filtered_by_value, attributes, value))
+                decision_tree(
+                    dataset_filtered_by_value, attributes, value_node)
         return new_node
 
 
@@ -64,6 +66,11 @@ def remove_repeated_values_of_list(list_of_values):
 def most_frequent(List):
     occurence_count = Counter(List)
     return occurence_count.most_common(1)[0][0]
+
+
+def print_tree(tree):
+    for pre, fill, node in RenderTree(tree):
+        print("%s%s" % (pre, node.name))
 
 
 if __name__ == "__main__":
